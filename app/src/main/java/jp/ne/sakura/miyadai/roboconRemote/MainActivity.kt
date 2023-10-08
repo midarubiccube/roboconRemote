6ac25ec3ad1f2262818cb8a5642a33de268cc999
mi.dai.tomo@gmail.com <mi.dai.tomo@gmail.com>
2023-10-08 13:04:13 +0900

    同期

--- app/src/main/java/jp/ne/sakura/miyadai/roboconRemote/MainActivity.kt
+++ app/src/main/java/jp/ne/sakura/miyadai/roboconRemote/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.RadioGroup
+import android.widget.SeekBar
 import android.widget.Switch
 import androidx.activity.ComponentActivity
 import com.longdo.mjpegviewer.MjpegView
@@ -17,14 +18,14 @@ import kotlin.math.sqrt
 
 class MainActivity : ComponentActivity() {
     var isconnect : Boolean = false
-    private var speed : Int = 230
     lateinit var webSocketClient : WebSocketClient
     lateinit var viewer : MjpegView
     lateinit var joyStickSurfaceView: JoyStickSurfaceView
     lateinit var horizontalStickSurfaceview: HorizontalStickSurfaceview
     lateinit var verticalSurfaceview: VerticalSurfaceview
     lateinit var Switch : Switch
-    private val STREAM_URL = "http://192.168.0.20:8000/?action=stream"
+    lateinit var seekBar: SeekBar
+    private val STREAM_URL = "http://192.168.0.37:81/stream"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,11 +37,13 @@ class MainActivity : ComponentActivity() {
 
         joyStickSurfaceView = findViewById(R.id.JoySticksurfaceView)
         Switch = findViewById(R.id.switch1)
-        val select_button = findViewById<RadioGroup>(R.id.speed_select)
+        seekBar = findViewById(R.id.seekBar)
         viewer = findViewById(R.id.mjpeg_view)
         horizontalStickSurfaceview = findViewById(R.id.horizontalStickSurfaceview)
         verticalSurfaceview = findViewById(R.id.verticalSurfaceview)
 
+        seekBar.min = 20
+        seekBar.max = 150
 
         timer.scheduleAtFixedRate(
             object : TimerTask() {
@@ -52,22 +55,11 @@ class MainActivity : ComponentActivity() {
             }, 100, 10
         )
 
-        select_button.setOnCheckedChangeListener { _, checkedId: Int ->
-            when (checkedId) {
-                R.id.radio_button_low -> {
-                    speed = 100
-                }
-                R.id.radio_button_high -> {
-                    speed = 230
-                }
-                else -> throw IllegalArgumentException("not supported")
-            }
-        }
-
         viewer.mode = MjpegView.MODE_FIT_WIDTH
         viewer.isAdjustHeight = true
         viewer.supportPinchZoomAndPan = false
         viewer.setUrl(STREAM_URL)
+        viewer.startStream()
 
         webSocketClient.send("Hello from Android")
 
@@ -93,10 +85,13 @@ class MainActivity : ComponentActivity() {
 
     private fun MakeSendData() : ByteArray {
         var bytes = ByteArray(0)
+        val speed = seekBar.progress
         bytes += (joyStickSurfaceView.getPosX * speed).makeByteArray()
         bytes += (joyStickSurfaceView.getPosY * speed).makeByteArray()
         bytes += (horizontalStickSurfaceview.sendX * speed).makeByteArray()
         bytes += (verticalSurfaceview.sendY * 128).makeByteArray()
+        bytes += (speed and 0xff).toByte()
+        bytes += if(Switch.isChecked) (1).toByte() else (0).toByte()
         return bytes
     }
     private fun Float.makeByteArray() : ByteArray{
