3e626878d821104c5e4fb1a31d462d70bbdeb0f4
midarubiccube <mi.dai.tomo@gmail.com>
2024-09-20 06:08:04 +0900

    親機

--- app/src/main/java/jp/ne/sakura/miyadai/roboconRemote/MainActivity.kt
+++ app/src/main/java/jp/ne/sakura/miyadai/roboconRemote/MainActivity.kt
@@ -13,18 +13,12 @@ import java.util.TimerTask
 
 class MainActivity : ComponentActivity() {
     var ESPisconnect : Boolean = false
-    var RPIisconnect : Boolean = false
     lateinit var ESPWebSocketClient : ESPWebSocketClient
-    lateinit var RPIWebSocketClient : RPIWebSocketClient
     lateinit var viewer : MjpegView
     lateinit var joyStickSurfaceView: JoyStickSurfaceView
     lateinit var horizontalStickSurfaceview: HorizontalStickSurfaceview
-    lateinit var horizontalStickSurfaceview1 : StickSurfaceview
-    lateinit var horizontalStickSurfaceview2 : StickSurfaceview
-    lateinit var horizontalStickSurfaceview3: HorizontalStickSurfaceview
     lateinit var verticalSurfaceview: VerticalSurfaceview
     lateinit var clawlerSwitch : Switch
-    lateinit var clawlerSwitch2 : Switch
     lateinit var SwitchSeppuku : Switch
     lateinit var speedseekBar: SeekBar
     private val STREAM_URL = "http://192.168.0.20:81/stream"
@@ -34,22 +28,14 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.activity_main)
 
         ESPWebSocketClient = ESPWebSocketClient(this)
-        RPIWebSocketClient = RPIWebSocketClient(this)
 
         val timer = Timer()
-
 
         joyStickSurfaceView = findViewById(R.id.JoySticksurfaceView)
         horizontalStickSurfaceview = findViewById(R.id.horizontalStickSurfaceview)
         verticalSurfaceview = findViewById(R.id.verticalSurfaceview)
-        horizontalStickSurfaceview1 = findViewById(R.id.horizontalStickSurfaceview2)
-        horizontalStickSurfaceview2 = findViewById(R.id.horizontalStickSurfaceview3)
-        horizontalStickSurfaceview3 = findViewById(R.id.horizontalStickSurfaceview4)
 
-
-        clawlerSwitch = findViewById(R.id.switch_clawler)
         SwitchSeppuku = findViewById(R.id.switch_seppuku)
-        clawlerSwitch2 = findViewById(R.id.switch_clawler2)
 
         speedseekBar = findViewById(R.id.speed_changer)
         viewer = findViewById(R.id.mjpeg_view)
@@ -72,33 +58,9 @@ class MainActivity : ComponentActivity() {
                         bytes += if (clawlerSwitch.isChecked) (1).toByte() else (0).toByte()
                         ESPWebSocketClient.send(bytes.toByteString())
                     }
-                    if (RPIisconnect) {
-                        var bytes = ByteArray(0)
-                        bytes += (verticalSurfaceview.sendY * 128).makeByteArray()
-                        bytes += (horizontalStickSurfaceview1.sendX * 100).makeByteArray()
-                        bytes += (horizontalStickSurfaceview2.sendX * 100).makeByteArray()
-                        bytes += (horizontalStickSurfaceview3.sendX * 50).makeByteArray()
-                        bytes += if (SwitchSeppuku.isChecked) (0).toByte() else (1).toByte()
-                        bytes += if (clawlerSwitch2.isChecked) (1).toByte() else (0).toByte()
-                        RPIWebSocketClient.send(bytes.toByteString())
-                    }
                 }
             }, 100, 25
         )
-
-        SwitchSeppuku.setOnCheckedChangeListener { _, isChecked ->
-          if(isChecked){
-              RPIWebSocketClient.send("crawleron")
-          }
-        }
-
-        SwitchSeppuku.setOnCheckedChangeListener { _, isChecked ->
-            if(isChecked){
-                RPIWebSocketClient.send("teston")
-            } else {
-                RPIWebSocketClient.send("testoff")
-            }
-        };
 
         viewer.mode = MjpegView.MODE_FIT_WIDTH
         viewer.isAdjustHeight = true
@@ -106,28 +68,24 @@ class MainActivity : ComponentActivity() {
         viewer.setUrl(STREAM_URL)
 
         ESPWebSocketClient.connect()
-        RPIWebSocketClient.connect()
     }
 
     override fun onPause() {
         super.onPause()
         Log.d("stop", "stop")
         ESPWebSocketClient.close()
-        RPIWebSocketClient.close()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         Log.d("stop", "stop")
         ESPWebSocketClient.close()
-        RPIWebSocketClient.close()
     }
 
     override fun onRestart() {
         super.onRestart()
         Log.d("restart", "restart")
         ESPWebSocketClient.connect()
-        RPIWebSocketClient.connect()
     }
 
     private fun Float.makeByteArray() : ByteArray{
